e2b0e6ddefd0980ada01b4c4c9f2101154efac7d
Yoga Racha <yogaracha39@gmail.com>
2025-02-15 00:45:22 +0700

    export notes implemented

--- src/api/collaborations/handler.js
+++ src/api/collaborations/handler.js
@@ -5,7 +5,8 @@ class CollaborationsHandler {
     this._validator = validator;
 
     this.postCollaborationHandler = this.postCollaborationHandler.bind(this);
-    this.deleteCollaborationHandler = this.deleteCollaborationHandler.bind(this);
+    this.deleteCollaborationHandler =
+      this.deleteCollaborationHandler.bind(this);
   }
 
   async postCollaborationHandler(request, h) {
@@ -14,11 +15,14 @@ class CollaborationsHandler {
     const { noteId, userId } = request.payload;
 
     await this._notesService.verifyNoteOwner(noteId, credentialId);
-    const collaborationId = await this._collaborationsService.addCollaboration(noteId, userId);
+    const collaborationId = await this._collaborationsService.addCollaboration(
+      noteId,
+      userId
+    );
 
     const response = h.response({
-      status: 'success',
-      message: 'Kolaborasi berhasil ditambahkan',
+      status: "success",
+      message: "Kolaborasi berhasil ditambahkan",
       data: {
         collaborationId,
       },
@@ -35,10 +39,10 @@ class CollaborationsHandler {
     await this._notesService.verifyNoteOwner(noteId, credentialId);
     await this._collaborationsService.deleteCollaboration(noteId, userId);
 
-    return {
-      status: 'success',
-      message: 'Kolaborasi berhasil dihapus',
-    };
+    return h.response({
+      status: "success",
+      message: "Kolaborasi berhasil dihapus",
+    });
   }
 }
 
