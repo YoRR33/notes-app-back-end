769b416df6832f42feddbb512c614b05eef29cd8
Yoga Racha <yogaracha39@gmail.com>
2025-02-17 23:46:54 +0700

    feat: add upload file function with multi part and use local storage

--- src/server.js
+++ src/server.js
@@ -3,6 +3,8 @@ require("dotenv").config();
 
 const Hapi = require("@hapi/hapi");
 const Jwt = require("@hapi/jwt");
+const path = require("path");
+const Inert = require("@hapi/inert");
 
 // notes
 const notes = require("./api/notes");
@@ -28,14 +30,22 @@ const CollaborationsValidator = require("./validator/collaborations");
 
 // Exports
 const _exports = require("./api/exports");
-const ProducerService = require("./services/rabbitmq/producerServices");
+const ProducerService = require("./services/rabbitmq/ProducerServices");
 const ExportsValidator = require("./validator/exports");
+
+// uploads
+const uploads = require("./api/uploads");
+const StorageService = require("./services/storage/StorageService");
+const UploadsValidator = require("./validator/uploads");
 
 const init = async () => {
   const collaborationsService = new CollaborationsService();
   const notesService = new NotesService(collaborationsService);
   const usersService = new UsersService();
   const authenticationsService = new AuthenticationsService();
+  const storageService = new StorageService(
+    path.resolve(__dirname, "api/uploads/file/images")
+  );
 
   const server = Hapi.server({
     port: process.env.PORT,
@@ -51,6 +61,9 @@ const init = async () => {
   await server.register([
     {
       plugin: Jwt,
+    },
+    {
+      plugin: Inert,
     },
   ]);
 
@@ -71,6 +84,7 @@ const init = async () => {
     }),
   });
 
+  // registrasi plugin internal
   await server.register([
     {
       plugin: notes,
@@ -108,6 +122,13 @@ const init = async () => {
       options: {
         service: ProducerService,
         validator: ExportsValidator,
+      },
+    },
+    {
+      plugin: uploads,
+      options: {
+        service: storageService,
+        validator: UploadsValidator,
       },
     },
   ]);
